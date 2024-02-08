6edeb9a41f811f07ba4f7cb46ec196a371d96330
jzuckerman1 <jzuckerman@vassar.edu>
2024-02-07 20:41:25 -0500

    Fixed Slider

--- main.js
+++ main.js
@@ -2,29 +2,21 @@ document.addEventListener("DOMContentLoaded", function (){
 let my__slider = tns({
     container : ".my__slider",
     // startIndex: 0,
-    "slideBy": "1",
+    slideBy: 1,
     "speed" : 400,
     "nav" : false,
-
+    loop: true,
     autoplay : true,
     controls : false,
     autoplayButtonOutput : false,
-    responsive : {
-        1600: {
-            items : 4,
-            gutter : 20
-        },
-        1024: {
-            items: 3,
-            gutter : 20
-        },
-        768: {
-            items: 2,
-            gutter: 20
-        },
-        480: {
-            items: 1
-        }
-        }
+    gutter : 30,
+    center : true,
+    controls : true,
+    responsive: {
+        1600: { items: 4 },
+        1024: { items: 3 },
+        768: { items: 2 },
+        480: { items: 1 }
+    }
     });
 });
