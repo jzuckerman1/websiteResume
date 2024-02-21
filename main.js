576be00baad69230e2e03ef1c5bdf2ea4d1421b2
jzuckerman1 <jzuckerman@vassar.edu>
2024-02-20 20:32:38 -0500

    Removed Test Files

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
-document.addEventListener("DOMContentLoaded", function (){
-let my__slider = tns({
-    container : ".my__slider",
-    // startIndex: 0,
-    slideBy: 1,
-    "speed" : 400,
-    "nav" : false,
+// document.addEventListener("DOMContentLoaded", function (){
+// let my__slider = tns({
+//     container : ".my__slider",
+//     // startIndex: 0,
+//     slideBy: 1,
+//     "speed" : 400,
+//     "nav" : false,
+//     loop: true,
+//     autoplay : true,
+//     controls : false,
+//     autoplayButtonOutput : false,
+//     gutter : 30,
+//     center : true,
+//     responsive: {
+//         1600: { items: 4 },
+//         1024: { items: 3 },
+//         768: { items: 2 },
+//         480: { items: 1 }
+//     }
+//     });
+// });
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
     loop: true,
-    autoplay : true,
-    controls : false,
-    autoplayButtonOutput : false,
-    gutter : 30,
-    center : true,
-    controls : true,
-    responsive: {
-        1600: { items: 4 },
-        1024: { items: 3 },
-        768: { items: 2 },
-        480: { items: 1 }
-    }
-    });
-});
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
